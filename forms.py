47e0b647487341ad9e1775b2a3e916cb65ad4d99
efemona.avre <efemona.avre@gmail.com>
2022-07-28 01:49:59 +0100

    initial login, register, forget forms

--- forms.py
+++ forms.py
@@ -1,7 +1,10 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from datetime import datetime
 from flask_ckeditor import CKEditorField
+from flask_wtf import FlaskForm
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, URL, Email, Length, InputRequired, ValidationError
+from wtforms.widgets import PasswordInput
 
 
 ##WTForm
@@ -13,19 +16,89 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
+# CREATE REGISTER FORM
 class RegisterForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    name = StringField("Name", validators=[DataRequired()])
-    submit = SubmitField("Sign Me Up!")
+    name = StringField('Full Name', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField('password', widget=PasswordInput(hide_value=False),
+                             validators=[DataRequired(), Length(min=8, max=32)])
+    confirm = PasswordField(label='repeat password',
+                            widget=PasswordInput(hide_value=False),
+                            validators=[DataRequired(), Length(min=8, max=32)])
+    terms = BooleanField(label="I accept the terms of use", validators=[DataRequired()])
+    submit = SubmitField(label="Register")
+
+    def validate_password(self, password):
+        if password.data != self.confirm.data:
+            raise ValidationError('Password mismatch')
+
+    def validate_email(self, user):
+        email = user.query.filter_by(email=self.email.data).first()
+        if email:
+            raise ValidationError('email address already registered')
+        return True
+
+    def register_user(self, db, user):
+        dictionary = {'name': self.name.data,
+                      'email': self.email.data,
+                      'password': generate_password_hash(self.password.data,
+                                                         method='pbkdf2:sha256',
+                                                         salt_length=8)
+                      }
+
+        new_user = user(dictionary)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email", validators=[DataRequired()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='password',
+                             widget=PasswordInput(hide_value=False),
+                             validators=[DataRequired(), Length(min=8, max=32, message="password must be between 8 and 32 Character long")])
+    remember = BooleanField('remember me')
     submit = SubmitField("Let Me In!")
+
+    def check_user(self, user_db,):
+        error = None
+        user = user_db.query.filter_by(email=self.email.data).first()
+        if not user:
+            error = "your email is incorrect or doesn't exit."
+        else:
+            if not check_password_hash(user.password, self.password.data):
+                error = "Invalid password"
+        return user, error
 
 
 class CommentForm(FlaskForm):
     comment_text = CKEditorField("Comment", validators=[DataRequired()])
     submit = SubmitField("Submit Comment")
+
+
+class SubscribeForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    submit = SubmitField("Subscribe")
+
+    def check_email_exist(self, subscribers):
+        email = subscribers.query.filter_by(email=self.email.data).first()
+        if email:
+            raise ValidationError('email address already subscribed')
+        return True
+
+    def confirm_email_validity(self):
+        # send email has generated url
+        # use smtplib
+        # TODO implement emailing for confirmation of subscription
+        pass
+
+
+class ResetPasswordForm(FlaskForm):
+    email = StringField('email', validators=[DataRequired(), Email()])
+    submit = SubmitField("Reset password")
+
+    def check_email_exist(self, subscribers):
+        email = subscribers.query.filter_by(email=self.email.data).first()
+        if not email:
+            raise ValidationError('email address does not exist')
+        return True
--- main.py
+++ main.py
@@ -10,18 +10,19 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
+from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm,ResetPasswordForm
 from flask_gravatar import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRETE_KEY')
+# app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config["SECRET_KEY"] = "MYSECRETEKEY"
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False,
                     base_url=None)
 
 ##CONNECT TO DB
-uri = os.getenv("DATABASE_URL", 'sqlite:///blog.db')  # or other relevant config var
+uri = os.getenv("DATABASE_URL", 'sqlite:///data.db')  # or other relevant config var
 if uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
 # rest of connection code using the connection string `uri`
@@ -74,14 +75,15 @@ def home():
 @app.route('/subscribe', methods=['GET', 'POST'])
 def subscribe():
     if request.method == "POST":
-        # flash subscribe successful message
         new_subscriber = Subscriber(email=request.form.get('email'))
         db.session.add(new_subscriber)
         try:
             db.session.commit()
+            flash(message="thank you for subscribing.", category="subscription")
         except IntegrityError as e:
             db.session.rollback()
-            # flash user already registered
+            flash(message="this email already subscribed", category="subscription")
+        return redirect(request.referrer)
 
     return render_template("index.html")
 
@@ -89,8 +91,41 @@ def subscribe():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     register_form = RegisterForm()
+    if register_form.validate_on_submit():
+        new_user = register_form.register_user(db, User)
+        login_user(new_user)
+        return redirect(url_for("home"))
 
     return render_template("register.html", form=register_form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user, error = login_form.check_user(User)
+        if error:
+            flash(message=error, category="login")
+        else:
+            login_user(user)
+            flash(message='You were successfully logged in', category="login")
+            return redirect(request.referrer)
+    return render_template("login.html", form=login_form)
+
+
+@app.route('/reset_password', methods=['GET', 'POST'])
+def reset_password():
+    reset_password_form = ResetPasswordForm()
+    if reset_password_form.validate_on_submit():
+        user, error = reset_password_form.check_email_exist(User)
+        if error:
+            flash(message=error, category="reset_password")
+        else:
+            # send email with confirmation url
+            flash(message='Instructions have been sent to your email', category="reset_password")
+            return redirect(request.referrer)
+    return render_template("reset_password.html", form=reset_password_form)
+
 
 
 if __name__ == "__main__":
